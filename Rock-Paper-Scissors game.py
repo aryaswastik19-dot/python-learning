1d5694bb5fdeb8583016fafb34e71505791da3ec
aryaswastik19-dot <aryaswastik19@gmail.com>
2025-12-11 22:38:40 +0530

    Add files via upload

---
+++ Rock-Paper-Scissors game.py
@@ -0,0 +1,18 @@
+# Rock-Paper-Scissors Game
+
+x = input("Choose Rock-Paper-Scissor:")
+
+if x == "Rock":
+    print("Paper!")
+    print("Hahaha, I win!")
+
+elif x == "Paper":
+    print("Scissor!")
+    print("Hahaha, I win!")
+
+elif x == "Scissor":
+    print("Rock!")
+    print("Hahaha, I win!")
+
+else:
+    print("Bruh!?")
